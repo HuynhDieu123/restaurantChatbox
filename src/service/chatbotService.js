aa8039eae945b68e29b1150fd351ef5cd7bceadc
Dieu <cdinh1078@gmail.com>
2025-01-04 21:15:58 +0700

    update

--- src/service/chatbotService.js
+++ src/service/chatbotService.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 import request from "request"
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const IMAGE_GET_STARTED = 'http://bit.ly/dieu-bot1';
+
 
 let callSendAPI = (response, sender_psid) => {
     // Construct the message body
@@ -39,7 +41,7 @@ let getUserName = (sender_psid) => {
             if (!err) {
                 body = JSON.parse(body);
                 console.log(body);
-                let username = `${body.first_name} ${body.last_name}`;
+                let username = `${body.last_name} ${body.first_name}`;
                 resolve(username);
             } else {
                 console.error("Unable to send message:" + err);
@@ -53,13 +55,54 @@ let handleGetStarted = (sender_psid) => {
     return new Promise(async (resolve, reject) => {
         try {
             let username = await getUserName(sender_psid);
-            let response = { "text": `Xin chào mừng bạn ${username} đến với nhà hàng của Điều` }
-            await callSendAPI(response, sender_psid);
+            let response1 = { "text": `Xin chào mừng bạn ${username} đến với nhà hàng của Điều` }
+            let response2 = sendGetStartedTemplate();
+
+            //send text message
+            await callSendAPI(response1, sender_psid);
+
+            //send generic tamplate message
+            await callSendAPI(response2, sender_psid)
+
             resolve('done');
         } catch (e) {
             reject(e)
         }
     })
+}
+
+let sendGetStartedTemplate = () => {
+    let response = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{
+                    "title": "Xin chào bạn đến với nhà hàng của Điều",
+                    "subtitle": "Dưới đây là các lựa chọn của nhà hàng.",
+                    "image_url": IMAGE_GET_STARTED,
+                    "buttons": [
+                        {
+                            "type": "postback",
+                            "title": "MENU CHÍNH",
+                            "payload": "MAIN_MENU",
+                        },
+                        {
+                            "type": "postback",
+                            "title": "ĐẶT BÀN",
+                            "payload": "RESERVE_TABLE",
+                        },
+                        {
+                            "type": "postback",
+                            "title": "HƯỚNG DẪN SỬ DỤNG BOT",
+                            "payload": "GUIDE_TO_USE",
+                        }
+                    ],
+                }]
+            }
+        }
+    }
+    return response;
 }
 
 module.exports = {
