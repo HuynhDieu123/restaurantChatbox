816aa0f6243f9e52c2e17c90a37e561533d5450d
Dieu <cdinh1078@gmail.com>
2025-01-09 18:10:17 +0700

    get-update

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -158,7 +158,7 @@ async function handlePostback(sender_psid, received_postback) {
     }
 
     // Send the message to acknowledge the postback
-    callSendAPI(sender_psid, response);
+    //callSendAPI(sender_psid, response);
 
 }
 
